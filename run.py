18518195e9ae843716a36a0ea877068aad8264e3
Onejae Sul <onejae.sul@gmail.com>
2017-01-18 08:12:04 +0000

    receving json from app engine

---
+++ run.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        firstname = request.json['first_name']
+        lastname = request.json['last_name']
+        return firstname + lastname[1]
+    else:
+        return "<h1> This is Index Page</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=80)
